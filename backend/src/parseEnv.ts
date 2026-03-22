cde6ec3f4e52720e8b1891304f64c888c08da8db
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-03-21 23:50:32 -0400

    backend: document all env vars

--- backend/src/parseEnv.ts
+++ backend/src/parseEnv.ts
@@ -1,8 +1,32 @@
 import { z } from "zod"
 
+/**
+ * The schema for the environment variables.
+ *
+ * Default values are coherent across all services for dev.
+ * Only keys that are 3rd party secret don't have default values, for safety.
+ */
 const envSchema = z.object({
+  /**
+   * PORT is injected by Railway and is not configurable, you can't rename this.
+   */
   PORT: z.coerce.number(),
+
+  /**
+   * See the infra docker-compose file for the dev db url
+   * postgres://<user>:<pwd>@localhost:<port>/<db>
+   */
   DATABASE_URL: z.string(),
+
+  /**
+   * Fetch the dev key from clerk and keep put it in your .env file
+   */
+  CLERK_PUBLISHABLE_KEY: z.string(),
+
+  /**
+   * Fetch the dev key from clerk and keep put it in your .env file
+   */
+  CLERK_SECRET_KEY: z.string(),
 })
 
 export function parseEnv(): z.infer<typeof envSchema> {
