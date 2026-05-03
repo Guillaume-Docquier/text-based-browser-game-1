8f2e0c1c5b009348887db25c456f1c7300ee13eb
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-05-02 23:32:19 -0400

    backend: comments

--- backend/src/tests/createPlayer.ts
+++ backend/src/tests/createPlayer.ts
@@ -3,6 +3,9 @@ import type { PlayerRow, PlayerRowInsert } from "#lib/db/players.repository.ts"
 import { playersTable } from "#lib/db/schema.ts"
 import { Assert } from "@guillaume-docquier/tools-ts"
 
+/**
+ * Inserts a player in the db and returns it.
+ */
 export async function createPlayer(db: NodePgDatabase, playerRowInsert: PlayerRowInsert): Promise<PlayerRow> {
   const player = (await db.insert(playersTable).values(playerRowInsert).returning())[0]
   Assert.isDefined(player)
--- backend/src/tests/vitest.setup.ts
+++ backend/src/tests/vitest.setup.ts
@@ -2,5 +2,7 @@ import { beforeAll } from "vitest"
 import { configureLogger } from "#lib/configureLogger.ts"
 
 beforeAll(async () => {
+  // nonBlocking otherwise the logs might not be flushed when the test finishes, making it hard to debug.
+  // maybe not ideal because it might change the timing of things compared to prod, but for now that'll have to do.
   await configureLogger({ scope: "test", nonBlocking: false })
 })
