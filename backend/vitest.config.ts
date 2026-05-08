d6c9ce0a05cf05f36b68e7c699834253e24bc1a7
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-05-07 22:02:48 -0400

    backend: setup codecov

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
+    coverage: {
+      exclude: ["src/**/*.test.ts", "src/tests/**"],
+      include: ["src/**/*.ts"],
+      provider: "v8",
+      reporter: ["text", "lcov"],
+      reportsDirectory: "./coverage",
+    },
     environment: "node",
     globals: false,
     include: ["src/**/*.test.ts"],
