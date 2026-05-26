81346e26bb6e6e4745406e98899ecaff9cf7ec84
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-05-25 21:10:02 -0400

    backend: simpler range dto schemas

--- backend/src/lib/Range.ts
+++ backend/src/lib/Range.ts
@@ -1,5 +1,5 @@
 import { z } from "zod"
-import { Range, type ExclusiveRange, type InclusiveRange, type Range as RangeType } from "@guillaume-docquier/tools-ts"
+import { Range, type ExclusiveRange, type InclusiveRange } from "@guillaume-docquier/tools-ts"
 
 /**
  * A base Range to represent what percentages are.
@@ -16,76 +16,68 @@ export const PercentageRange = Range.createMaxInclusive({
   }),
 })
 
-export const InclusiveFloatRangeDto: z.ZodType<InclusiveRange<"float">> = z.lazy(() =>
-  z
-    .object({
-      type: z.literal("MaxInclusive"),
-      numericType: z.literal("float"),
-      min: z.number(),
-      maxInclusive: z.number(),
-      limits: FloatRangeDto.optional(),
-    })
-    .superRefine((range, context) => {
-      const invalidReason = Range.validate(range)
-      if (invalidReason !== undefined) {
-        context.addIssue({ code: "custom", message: invalidReason })
-      }
-    }),
-)
+export const InclusiveFloatRangeDto: z.ZodType<InclusiveRange<"float">> = z
+  .object({
+    type: z.literal("MaxInclusive"),
+    numericType: z.literal("float"),
+    min: z.number(),
+    maxInclusive: z.number(),
+    limits: z.lazy(() => FloatRangeDto).exactOptional(),
+  })
+  .superRefine((range, context) => {
+    const invalidReason = Range.validate(range)
+    if (invalidReason !== undefined) {
+      context.addIssue({ code: "custom", message: invalidReason })
+    }
+  })
 
-export const InclusiveIntegerRangeDto: z.ZodType<InclusiveRange<"integer">> = z.lazy(() =>
-  z
-    .object({
-      type: z.literal("MaxInclusive"),
-      numericType: z.literal("integer"),
-      min: z.number(),
-      maxInclusive: z.number(),
-      limits: IntegerRangeDto.optional(),
-    })
-    .superRefine((range, context) => {
-      const invalidReason = Range.validate(range)
-      if (invalidReason !== undefined) {
-        context.addIssue({ code: "custom", message: invalidReason })
-      }
-    }),
-)
+export const InclusiveIntegerRangeDto: z.ZodType<InclusiveRange<"integer">> = z
+  .object({
+    type: z.literal("MaxInclusive"),
+    numericType: z.literal("integer"),
+    min: z.number(),
+    maxInclusive: z.number(),
+    limits: z.lazy(() => IntegerRangeDto).exactOptional(),
+  })
+  .superRefine((range, context) => {
+    const invalidReason = Range.validate(range)
+    if (invalidReason !== undefined) {
+      context.addIssue({ code: "custom", message: invalidReason })
+    }
+  })
 
-export const ExclusiveFloatRangeDto: z.ZodType<ExclusiveRange<"float">> = z.lazy(() =>
-  z
-    .object({
-      type: z.literal("MaxExclusive"),
-      numericType: z.literal("float"),
-      min: z.number(),
-      maxExclusive: z.number(),
-      limits: FloatRangeDto.optional(),
-    })
-    .superRefine((range, context) => {
-      const invalidReason = Range.validate(range)
-      if (invalidReason !== undefined) {
-        context.addIssue({ code: "custom", message: invalidReason })
-      }
-    }),
-)
+export const ExclusiveFloatRangeDto: z.ZodType<ExclusiveRange<"float">> = z
+  .object({
+    type: z.literal("MaxExclusive"),
+    numericType: z.literal("float"),
+    min: z.number(),
+    maxExclusive: z.number(),
+    limits: z.lazy(() => FloatRangeDto).exactOptional(),
+  })
+  .superRefine((range, context) => {
+    const invalidReason = Range.validate(range)
+    if (invalidReason !== undefined) {
+      context.addIssue({ code: "custom", message: invalidReason })
+    }
+  })
 
-export const ExclusiveIntegerRangeDto: z.ZodType<ExclusiveRange<"integer">> = z.lazy(() =>
-  z
-    .object({
-      type: z.literal("MaxExclusive"),
-      numericType: z.literal("integer"),
-      min: z.number(),
-      maxExclusive: z.number(),
-      limits: IntegerRangeDto.optional(),
-    })
-    .superRefine((range, context) => {
-      const invalidReason = Range.validate(range)
-      if (invalidReason !== undefined) {
-        context.addIssue({ code: "custom", message: invalidReason })
-      }
-    }),
-)
+export const ExclusiveIntegerRangeDto: z.ZodType<ExclusiveRange<"integer">> = z
+  .object({
+    type: z.literal("MaxExclusive"),
+    numericType: z.literal("integer"),
+    min: z.number(),
+    maxExclusive: z.number(),
+    limits: z.lazy(() => IntegerRangeDto).exactOptional(),
+  })
+  .superRefine((range, context) => {
+    const invalidReason = Range.validate(range)
+    if (invalidReason !== undefined) {
+      context.addIssue({ code: "custom", message: invalidReason })
+    }
+  })
 
-const FloatRangeDto: z.ZodType<RangeType<"float">> = z.union([InclusiveFloatRangeDto, ExclusiveFloatRangeDto])
+const FloatRangeDto: z.ZodType<Range<"float">> = z.union([InclusiveFloatRangeDto, ExclusiveFloatRangeDto])
 
-const IntegerRangeDto: z.ZodType<RangeType<"integer">> = z.union([InclusiveIntegerRangeDto, ExclusiveIntegerRangeDto])
+const IntegerRangeDto: z.ZodType<Range<"integer">> = z.union([InclusiveIntegerRangeDto, ExclusiveIntegerRangeDto])
 
-export const RangeDto: z.ZodType<RangeType> = z.union([FloatRangeDto, IntegerRangeDto])
+export const RangeDto: z.ZodType<Range> = z.union([FloatRangeDto, IntegerRangeDto])
