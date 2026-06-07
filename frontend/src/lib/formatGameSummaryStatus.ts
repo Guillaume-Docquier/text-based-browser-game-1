1d0d84afe823dc96c80fef1260db9868bee2994a
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-06-07 14:22:29 -0400

    backend: nuke db

--- frontend/src/lib/formatGameSummaryStatus.ts
+++ frontend/src/lib/formatGameSummaryStatus.ts
@@ -3,7 +3,7 @@ import type { GameSummaryStatus } from "@api-types"
 export function formatGameSummaryStatus(gameStatus: GameSummaryStatus): string {
   switch (gameStatus) {
     case "WAITING_FOR_PLAYERS":
-      return "Waiting for more accounts"
+      return "Waiting for more players"
     case "READY_TO_START":
       return "Ready to start"
     case "STARTED":
