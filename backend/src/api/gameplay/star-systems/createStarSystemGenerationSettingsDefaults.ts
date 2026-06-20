3447e406a052694ba3c679223fedc7a77f533e7f
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-06-20 15:20:36 -0400

    backend: tweaked default values

--- backend/src/api/gameplay/star-systems/createStarSystemGenerationSettingsDefaults.ts
+++ backend/src/api/gameplay/star-systems/createStarSystemGenerationSettingsDefaults.ts
@@ -4,8 +4,8 @@ import { randomUInt32 } from "#lib/randomUInt32.ts"
 
 export function createStarSystemGenerationSettingsDefaults(): StarSystemGenerationSettings {
   return {
-    planetDensity: Range.create({ numericType: "float", maxBoundType: "inclusive", min: 0.4, max: 0.6 }),
-    nbPlanets: Range.integer({ min: 9, max: 11 }),
+    planetDensity: Range.create({ numericType: "float", maxBoundType: "inclusive", min: 0.75, max: 0.85 }),
+    nbPlanets: Range.integer({ min: 5, max: 8 }),
     nbMoonsPerPlanet: Range.integer({ min: 1, max: 3 }),
     nbAsteroidBelts: Range.integer({ min: 1, max: 1 }),
     nbAsteroidsPerSector: Range.integer({ min: 1, max: 3 }),
