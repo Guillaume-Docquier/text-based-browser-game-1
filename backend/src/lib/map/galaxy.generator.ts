20a13624acffe330c6b8d4bf6263821c405b2129
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-08-05 00:03:04 -0400

    remove the grid align because the output doesn't look random anymore, but we still keep at most 1 star per cell

--- backend/src/lib/map/galaxy.generator.ts
+++ backend/src/lib/map/galaxy.generator.ts
@@ -20,8 +20,7 @@ type PointsGenerator = (options: { size: number }) => Point2D[]
 
 /**
  * Creates a square galaxy of the given size in light years.
- * All stars will be grid aligned.
- * No stars will occupy the same grid cell, meaning the minimum distance between any 2 stars is 1 light year.
+ * No 2 stars will occupy the same grid cell to allow unique cell based coordinates.
  *
  * We need to know the galaxy size because generators might produces values that will be out of bounds.
  */
@@ -29,17 +28,13 @@ export function galaxyGenerator({ size, pointsGenerator }: { size: number; point
   const uniquePoints = new Map<string, Point2D>()
   const points = pointsGenerator({ size })
   for (const point of points) {
-    // Grid align
-    point.x = Math.round(point.x)
-    point.y = Math.round(point.y)
-
     // Remove out of bounds
     if (point.x < 0 || point.y >= size) {
       continue
     }
 
-    // Remove duplicates
-    uniquePoints.set(`${point.x.toFixed(0)}-${point.y.toFixed(0)}`, point)
+    // Keep at most 1 point per cell
+    uniquePoints.set(`${Math.floor(point.x)}-${Math.floor(point.y)}`, point)
   }
 
   return {
