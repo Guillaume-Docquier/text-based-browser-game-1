9cd1bbf3aa94ec8576a875f0a2fcf02c6c326a2a
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-08-13 00:21:56 -0400

    is this it?

--- backend/src/lib/rules-engine/phases/resolvers/resolveIncomePhase.ts
+++ backend/src/lib/rules-engine/phases/resolvers/resolveIncomePhase.ts
@@ -1,8 +1,10 @@
+import { resolveEffect } from "#lib/rules-engine/effects/resolveEffect.ts"
 import { IncomeMechanic } from "#lib/rules-engine/mechanics/IncomeMechanic.ts"
 import type { PhaseContext } from "#lib/rules-engine/phases/PhaseContext.ts"
 
 export function resolveIncomePhase(context: PhaseContext): void {
   const incomeEffects = context.effects.getEffectsOfType(IncomeMechanic.type)
-  for (const incomeAction of incomeEffects) {
+  for (const incomeEffect of incomeEffects) {
+    resolveEffect(context, incomeEffect)
   }
 }
