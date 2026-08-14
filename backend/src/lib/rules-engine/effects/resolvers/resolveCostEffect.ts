20845e7402a45cbe58e681b6bf8f25c9cfd25b20
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-08-13 20:32:39 -0400

    effect resolvers

--- backend/src/lib/rules-engine/effects/resolvers/resolveCostEffect.ts
+++ backend/src/lib/rules-engine/effects/resolvers/resolveCostEffect.ts
@@ -1,5 +1,17 @@
+import { Assert } from "@guillaume-docquier/tools-ts"
 import type { Effect } from "#lib/rules-engine/effects/Effect.ts"
 import type { CostMechanic } from "#lib/rules-engine/mechanics/implementations/CostMechanic.ts"
 import type { PhaseContext } from "#lib/rules-engine/phases/PhaseContext.ts"
 
-export function resolveCostEffect(_context: PhaseContext, _effect: Effect<CostMechanic>): void {}
+/**
+ * Does not validate that the resource won't go into negative by design.
+ */
+export function resolveCostEffect(context: PhaseContext, effect: Effect<CostMechanic>): void {
+  const player = context.state.players[effect.targets.self]
+  Assert.isDefined(player)
+
+  const resourceCount = player.resources[effect.mechanic.resourceType]
+  Assert.isDefined(resourceCount)
+
+  player.resources[effect.mechanic.resourceType] = resourceCount - effect.mechanic.quantity
+}
--- backend/src/lib/rules-engine/effects/resolvers/resolveIncomeEffect.ts
+++ backend/src/lib/rules-engine/effects/resolvers/resolveIncomeEffect.ts
@@ -1,5 +1,14 @@
+import { Assert } from "@guillaume-docquier/tools-ts"
 import type { Effect } from "#lib/rules-engine/effects/Effect.ts"
 import type { IncomeMechanic } from "#lib/rules-engine/mechanics/implementations/IncomeMechanic.ts"
 import type { PhaseContext } from "#lib/rules-engine/phases/PhaseContext.ts"
 
-export function resolveIncomeEffect(_context: PhaseContext, _effect: Effect<IncomeMechanic>): void {}
+export function resolveIncomeEffect(context: PhaseContext, effect: Effect<IncomeMechanic>): void {
+  const player = context.state.players[effect.targets.self]
+  Assert.isDefined(player)
+
+  const resourceCount = player.resources[effect.mechanic.resourceType]
+  Assert.isDefined(resourceCount)
+
+  player.resources[effect.mechanic.resourceType] = resourceCount + effect.mechanic.quantity
+}
