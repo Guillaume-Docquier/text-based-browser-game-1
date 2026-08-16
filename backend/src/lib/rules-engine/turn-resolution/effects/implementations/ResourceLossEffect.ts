f0fd19de7b0adf0f5c43e1691c42e49c0be298e3
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-08-16 14:17:25 -0400

    oop effects and shorter scripts

---
+++ backend/src/lib/rules-engine/turn-resolution/effects/implementations/ResourceLossEffect.ts
@@ -0,0 +1,23 @@
+import { Assert } from "@guillaume-docquier/tools-ts"
+import type { ActionSubmission } from "#lib/rules-engine/action-submission/ActionSubmission.ts"
+import type { ResourceLossMechanic } from "#lib/rules-engine/ruleset-model/mechanics/implementations/ResourceLossMechanic.ts"
+import { Effect } from "#lib/rules-engine/turn-resolution/effects/Effect.ts"
+import type { TurnContext } from "#lib/rules-engine/turn-resolution/TurnContext.ts"
+
+export class ResourceLossEffect extends Effect {
+  private readonly mechanic: ResourceLossMechanic
+  private readonly targetPlayerId: string
+
+  public constructor(mechanic: ResourceLossMechanic, targets: ActionSubmission["targets"]) {
+    super(mechanic.type)
+    this.mechanic = mechanic
+    this.targetPlayerId = targets[mechanic.targets.player.tag]
+  }
+
+  protected override doResolve(context: TurnContext): void {
+    const player = context.state.players[this.targetPlayerId]
+    Assert.isDefined(player)
+
+    player.resources[this.mechanic.resourceType] -= this.mechanic.quantity
+  }
+}
