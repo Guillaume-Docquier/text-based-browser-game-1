49ee6e1af2dc41a80da661e76298c60be55c3b11
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-09-04 01:35:34 -0400

    revert

--- backend/src/lib/db/schema.ts
+++ backend/src/lib/db/schema.ts
@@ -33,7 +33,7 @@ import type { RulesetRulesJson } from "#lib/rulesets/rulesets.repository.ts"
  * Turns a fake enum (const {} as const) into a pgEnum compatible parameter.
  * This is just type gymnastics
  */
-export function pgEnumify<TEnumLike extends string>(enumLike: Record<string, TEnumLike>): [TEnumLike, ...TEnumLike[]] {
+function pgEnumify<TEnumLike extends string>(enumLike: Record<string, TEnumLike>): [TEnumLike, ...TEnumLike[]] {
   // oxlint-disable-next-line typescript/no-unsafe-type-assertion -- This is trusted code
   return Object.values(enumLike) as [TEnumLike, ...TEnumLike[]]
 }
