4d4ba1cca19227210980fe99e71cf7c1fc7b3425
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-09-07 11:12:46 -0400

    build fleet directives

--- backend/src/lib/rulesets/standard/StandardRuleset.ts
+++ backend/src/lib/rulesets/standard/StandardRuleset.ts
@@ -2,6 +2,7 @@ import { branded } from "@guillaume-docquier/tools-ts"
 import { indexById } from "#lib/indexById.ts"
 import { ResourceType } from "#lib/rules-engine/ruleset-model/mechanics/ResourceType.ts"
 import type { Ruleset } from "#lib/rules-engine/ruleset-model/Ruleset.ts"
+import { BuildFleetExceptional, BuildFleetImproved, BuildFleetStandard } from "#lib/rulesets/standard/action-definitions/build-fleet.ts"
 import { GainEnergy } from "#lib/rulesets/standard/action-definitions/gain-energy.ts"
 import { GainFuel } from "#lib/rulesets/standard/action-definitions/gain-fuel.ts"
 import { GainInfluence } from "#lib/rulesets/standard/action-definitions/gain-influence.ts"
@@ -15,7 +16,16 @@ export const StandardRuleset: Ruleset = {
   id: branded("core_standard_v1"),
   name: "Standard V1",
   isDefault: true,
-  actionDefinitions: indexById([GainInfluence, WinTheGame, GainEnergy, GainFuel, GainMetal]),
+  actionDefinitions: indexById([
+    GainInfluence,
+    WinTheGame,
+    GainEnergy,
+    GainFuel,
+    GainMetal,
+    BuildFleetStandard,
+    BuildFleetImproved,
+    BuildFleetExceptional,
+  ]),
   startingResources: {
     [ResourceType.INFLUENCE]: 3,
     [ResourceType.METAL]: 2,
