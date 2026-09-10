565f44df4d49c174d7281e800b737dd142083d94
Guillaume Docquier <guillaumedocquier@hotmail.com>
2026-09-10 00:13:35 -0400

    fix fleet ids

--- backend/src/lib/db/fleets/FleetId.ts
+++ backend/src/lib/db/fleets/FleetId.ts
@@ -2,7 +2,7 @@ import { branded, type Branded } from "@guillaume-docquier/tools-ts"
 import { uuid } from "drizzle-orm/pg-core"
 import { z } from "zod"
 
-export type FleetId = Branded<string, "FleetId">
+export type FleetId = Branded<"FleetId", string>
 export const FleetId = z.string().transform(branded<FleetId>)
 
 // oxlint-disable-next-line typescript/explicit-function-return-type -- Let drizzle inference do the work
